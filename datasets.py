a98042df9714ccb3d116a293bc0240aeeb95fe23
Moses Bomera <mosesbomera@gmail.com>
2021-06-12 13:43:32 +0300

    Add LunaDataset class

--- datasets.py
+++ datasets.py
@@ -155,3 +155,65 @@ def getCtRawCandidate(series_uid, center_xyz, width_irc):
     ct_chunk, center_irc = ct.getRawCandidate(center_xyz, width_irc)
     return ct_chunk, center_irc
 
+
+class LunaDataset(Dataset):
+    def __init__(
+        self,
+        val_stride=0,
+        isValSet_bool=None,
+        series_uid=None,
+    ):
+        self.candidateInfo_list = copy.copy(getCandidateInfoList())
+
+        # Extract nodule info for a given series_uid (CT).
+        if series_uid:
+            self.candidateInfo_list = [
+                x for x in self.candidateInfo_list if x.series_uid == series_uid
+            ]
+        
+        if isValSet_bool:
+            assert val_stride > 0, val_stride
+            self.candidateInfo_list = self.candidateInfo_list[::val_stride] # Starting with the first candidate, get every 
+                                                                            # val_stride candidate
+            assert self.candidateInfo_list
+        elif val_stride > 0:
+            del self.candidateInfo_list[::val_stride]
+            assert self.candidateInfo_list
+
+        log.info("{!r}: {} {} samples".format(
+            self,
+            len(self.candidateInfo_list),
+            "validation" if isValSet_bool else "training"
+        ))
+
+        def __len__(self):
+            return len(self.getCandidateInfoList)
+
+        def __getitem__(self, ndx):
+            # Index the, or so to say, get the candidate from the list.
+            candidateInfo_tup = self.candidateInfo_list[ndx]
+            width_irc = (32, 48, 48)
+
+            candidate_a, center_irc = getCtRawCandidate(
+                candidateInfo_tup.series_uid,  
+                candidateInfo_tup.center_xyz, 
+                width_irc
+            )
+
+            candidate_t = torch.from_numpy(candidate_a)
+            candidate_t = candidate_t.to(torch.float32)
+            candidate_t = candidate_t.unsqueeze(0)
+
+            pos_t = torch.tensor([
+                    not candidateInfo_tup.isNodule_bool,
+                    candidateInfo_tup.isNodule_bool
+                ],
+                dtype=torch.long
+            )
+
+            return (
+                candidate_t,
+                pos_t,
+                candidateInfo_tup.series_uid,
+                torch.tensor(center_irc)
+            )
