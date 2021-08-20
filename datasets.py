ab4e9c3cd840fa8e13d5f2bd84177e341ddd521c
Moses Bomera <mosesbomera@gmail.com>
2021-08-20 23:15:20 +0300

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -41,7 +41,7 @@ def getCandidateInfoList(requireOnDisk_bool=True):
     # We construct a set with all series_uids that are present on disk.
     # This will let us use the data, even if we haven't downloaded all of
     # the subsets yet.
-    mhd_list = glob.glob(f'{data_dir}/subset*/subset*/*.mhd')
+    mhd_list = glob.glob(f'{data_dir}/subset*/*.mhd')
     presentOnDisk_set = {os.path.split(p)[-1][:-4] for p in mhd_list}
 
     diameter_dict = {}
